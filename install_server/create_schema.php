e53e5566282d04e68f47ad62e15a2de78cf00493
Peter Sági <psagi@freemail.hu>
2010-01-01 14:39:50 +0000

    Init the schema.

--- install_server/create_schema.php
+++ install_server/create_schema.php
@@ -2,16 +2,21 @@
 <?php
    require_once(dirname(__FILE__) . '/../server/CloudBankServer.php');
    require_once(dirname(__FILE__) . '/../server/SchemaDef.php');
+   require_once(dirname(__FILE__) . '/../server/LedgerAccount.php');
 
    $v_dBConnection = CloudBankServer::GetDBConnection();
    foreach (SchemaDef::CreateSchemaStatements() as $v_sQLStatement) {
-      try {
+//      try {
 	 $v_dBConnection->exec($v_sQLStatement);
-      }
+//      }
+/*      
       catch (PDOException $v_exception) {
 	 exit(
-	    "Execution of SQL statement failed: $v_exception->getMessage()\n"
+	    "Execution of SQL statement failed: " . $v_exception->getMessage() .
+	    "\n"
 	 );
       }
+*/
    }
+   LedgerAccount::CreateBeginningAccount();
 ?>
