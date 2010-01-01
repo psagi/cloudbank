46b9d376cac87f07035db73a2a1d484aefb699dd
Peter Sági <psagi@freemail.hu>
2010-01-01 14:41:39 +0000

    Configuration using .ini file, PDO to throw exceptions on DB errors, syntax fixes.

--- server/CloudBankServer.php
+++ server/CloudBankServer.php
@@ -1,6 +1,5 @@
 <?php 
    require_once('SDO/DAS/Relational.php');
-   require_once(dirname(__FILE__) . '/../conf/db.conf');
    require_once(dirname(__FILE__) . '/SchemaDef.php');
 
    class CloudBankServer {
@@ -12,23 +11,26 @@
 	 return self::$r_instance;
       }
       public static function GetDBConnection() {
-	 global $g_dBConf;
-	 try {
+	 $v_dBConf = parse_ini_file(dirname(__FILE__) . '/../conf/db.ini');
+//	 try {
 	    $v_dBConnection = new PDO(
-	       $g_dBConf['dsn'], $g_dBConf['user'], $g_dBConf['passwd']
+	       $v_dBConf['dsn'], $v_dBConf['user'], $v_dBConf['passwd']
 	    );
-	 }
+/*	 }
 	 catch (PDOException $v_exception) {
 	    exit("Connection failed: $v_exception->getMessage()\n");
 	 }
-
+*/
+	 $v_dBConnection->setAttribute(
+	    PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION
+	 );
 	 return $v_dBConnection;
       } 
 
       private static $r_instance;
    
       private function __construct() {
-	 $this->r_dBConnetcion = self::GetDBConnection();
+	 $this->r_dBConnection = self::GetDBConnection();
 	 $this->r_dAS = (
 	    new SDO_DAS_Relational(
                SchemaDef::Metadata(), 'ledger_account',
@@ -36,9 +38,6 @@
             )
 	 );
 	 $v_dASQuery = SchemaDef::DASQuery();
-//var_dump($this->r_dBConnection, $this->r_dBConnection->getAttribute(PDO::ATTR_SERVER_INFO), $v_dASQuery);	 
-//var_dump($this->r_dBConnection->prepare('SELECT * FROM test'));
-//exit();
 	 $this->r_rootDO = (
 	    $this->r_dAS->executeQuery(
 	       $this->r_dBConnection, $v_dASQuery['query'], $v_dASQuery['array']
@@ -47,8 +46,8 @@
       }
       private function __clone() { }
 
-      public function dBConnection()	{ return $r_dBConnection;	}
-      public function rootDO()		{ return $r_rootDO;		}
+      public function dBConnection()	{ return $this->r_dBConnection;	}
+      public function rootDO()		{ return $this->r_rootDO;	}
       public function applyChanges() {
 	 return (
 	    $this->r_dAS->applyChanges($this->r_dBConnection, $this->r_rootDO)
