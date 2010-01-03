e77eb194198e5d625b367443b175586c13bdef3e
Peter Sági <psagi@freemail.hu>
2010-01-03 00:49:17 +0000

    Got rid of SDO_DAS, UUID(), DB connection handling encapsulated, execQuery(), transaction handling

--- server/CloudBankServer.php
+++ server/CloudBankServer.php
@@ -1,19 +1,26 @@
 <?php 
-   require_once('SDO/DAS/Relational.php');
    require_once(dirname(__FILE__) . '/SchemaDef.php');
 
    class CloudBankServer {
       public static function Singleton() {
-	 if (!isset(self::$r_instance)) {
-	    self::$r_instance = new CloudBankServer;
+	 static $v_instance = NULL;
+	 if (!isset($v_instance)) {
+	    $v_instance = new CloudBankServer;
 	 }
-	 
-	 return self::$r_instance;
+	 return $v_instance;
       }
-      public static function GetDBConnection() {
+      public static function UUID() {
+	 static $v_UUIDGenerator = NULL;
+	 if (!$v_UUIDGenerator) uuid_create(&$v_UUIDGenerator);
+	 uuid_make($v_UUIDGenerator, UUID_MAKE_V1);
+	 uuid_export($v_UUIDGenerator, UUID_FMT_SIV, &$v_uuid);
+	 return $v_uuid;
+      }
+
+      private function __construct() {
 	 $v_dBConf = parse_ini_file(dirname(__FILE__) . '/../conf/db.ini');
 //	 try {
-	    $v_dBConnection = new PDO(
+	 $this->r_dBConnection = new PDO(
 	       $v_dBConf['dsn'], $v_dBConf['user'], $v_dBConf['passwd']
 	    );
 /*	 }
@@ -21,41 +28,29 @@
 	    exit("Connection failed: $v_exception->getMessage()\n");
 	 }
 */
-	 $v_dBConnection->setAttribute(
+	 $this->r_dBConnection->setAttribute(
 	    PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION
-	 );
-	 return $v_dBConnection;
-      } 
-
-      private static $r_instance;
-   
-      private function __construct() {
-	 $this->r_dBConnection = self::GetDBConnection();
-	 $this->r_dAS = (
-	    new SDO_DAS_Relational(
-               SchemaDef::Metadata(), 'ledger_account',
-	       SchemaDef::ContainmentMetadata()
-            )
-	 );
-	 $v_dASQuery = SchemaDef::DASQuery();
-	 $this->r_rootDO = (
-	    $this->r_dAS->executeQuery(
-	       $this->r_dBConnection, $v_dASQuery['query'], $v_dASQuery['array']
-	    )
 	 );
       }
       private function __clone() { }
 
-      public function dBConnection()	{ return $this->r_dBConnection;	}
-      public function rootDO()		{ return $this->r_rootDO;	}
-      public function applyChanges() {
-	 return (
-	    $this->r_dAS->applyChanges($this->r_dBConnection, $this->r_rootDO)
-	 );
+      public function execQuery($p_sQL, $p_bindArray = NULL) {
+	 $v_statement = $this->r_dBConnection->prepare($p_sQL);
+	 $v_statement->execute($p_bindArray);
+	 return $v_statement->fetchAll();
+      }
+      public function beginTransaction() {
+	 $this->r_dBConnection->beginTransaction();
+      }
+/*
+      public function rollBackTransaction() {
+	 $this->r_dBConnection->rollBack();
+      }
+*/
+      public function commitTransaction() {
+	 $this->r_dBConnection->commit();
       }
 
       private $r_dBConnection;
-      private $r_dAS;
-      private $r_rootDO;
    }
 ?>
