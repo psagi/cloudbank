0298b7d8e0214564ae1f287acf76675a00f8e110
Peter Sági <psagi@freemail.hu>
2010-01-03 00:54:09 +0000

    input validation on published operations, got rid of SDO_DAS, transaction handling, UUID() moved to CloudBankServer

--- server/LedgerAccountService.php
+++ server/LedgerAccountService.php
@@ -1,5 +1,6 @@
 <?php
    require_once('CloudBankServer.php');
+   require_once('Event.php');
    include('SCA/SCA.php');
 
    /**
@@ -15,7 +16,7 @@
 
       /**
 	 @param string $p_name	The name of the account
-	 @param string $p_date	The date of creation of the account
+	 @param string $p_date	The date of creation of the account (YYYY-MM-DD)
 	 @param float $p_beginningBalance	\
 	    The beginning balance of the account 
 	 @return bool		Success
@@ -23,16 +24,13 @@
       public static function CreateAccount(
 	 $p_name, $p_date, $p_beginningBalance
       ) {
-	 $v_ledgerAccount = self::CreateLedgerAccount($p_name, self::Account);
-	 $v_beginningEvent = $v_ledgerAccount->createDataObject('event');
-	 $v_beginningEvent->date = $p_date;
-	 $v_beginningEvent->description = self::BeginningEvntDesc;
-	 $v_beginningEvent->credit_ledger_account_id = (
-	    $v_ledgerAccount["ledger_account[type=self::Beginning]"]->id
+	 CloudBankServer::Singleton()->beginTransaction();
+	 $v_accntID = self::CreateLedgerAccount($p_name, self::Account);
+	 Event::CreateEvent_internal(
+	    $p_date, self::BeginningEvntDesc, $v_accntID,
+	    self::GetBeginningAccountID(), $p_beginningBalance
 	 );
-	 $beginningEvent->amount = $p_beginningBalance;
-	 CloudBankServer::Singleton()->applyChanges();
-
+	 CloudBankServer::Singleton()->commitTransaction();
 	 return true;
       }
 
@@ -41,36 +39,65 @@
 	 @return bool		Success
       */
       public static function CreateCategory($p_name) {
+	 CloudBankServer::Singleton()->beginTransaction();
 	 self::CreateLedgerAccount($p_name, self::Category);
-	 CloudBankServer::Singleton()->applyChanges();
-
+	 CloudBankServer::Singleton()->commitTransaction();
 	 return true;
       }
-
+      
+      /* Note that although this method is not annotated, SCA generates an
+	 operation for it in the WSDL. However this operation is probably
+	 unusable due to the missing annotations - and so type definitions in
+	 the WSDL.
+      */
       public static function CreateBeginningAccount() {
+	 CloudBankServer::Singleton()->beginTransaction();
 	 self::CreateLedgerAccount(self::BeginningAccntName, self::Beginning);
-	 CloudBankServer::Singleton()->applyChanges();
+	 CloudBankServer::Singleton()->commitTransaction();
       }
 
-      private static function UUID() {
-	 static $v_UUIDGenerator = NULL;
-	 if (!$v_UUIDGenerator) uuid_create(&$v_UUIDGenerator);
-	 uuid_make($v_UUIDGenerator, UUID_MAKE_V1);
-	 uuid_export($v_UUIDGenerator, UUID_FMT_SIV, &$v_uuid);
-	 return $v_uuid;
+      private static function GetBeginningAccountID() {
+	 $v_result = (
+	    CloudBankServer::Singleton()->execQuery(
+	       'SELECT id FROM ledger_account WHERE type = :type',
+	       array(':type' => self::Beginning)
+	    )
+	 );
+	 return $v_result[0]['id'];
+      }
+
+      private static function DoesExist($p_name, $p_type) {
+	 return (
+	    count(
+	       CloudBankServer::Singleton()->execQuery(
+		  '
+		     SELECT 1
+			FROM ledger_account
+			WHERE name = :name AND type = :type
+		  ', array(':name' => $p_name, ':type' => $p_type)
+	       )
+	    ) > 0
+	 );
       }
 
       private static function CreateLedgerAccount($p_name, $p_type) {
-	 $v_ledgerAccount = (
-	    CloudBankServer::Singleton()->rootDO()->createDataObject(
-	       'ledger_account'
-	    )
+	 if (!SchemaDef::IsValidLedgerAccountName($p_name)) {
+	    throw new Exception("Invalid LedgerAccount name ($p_name)");
+	 }
+	 if (self::DoesExist($p_name, $p_type)) {
+	    throw
+	       new Exception("LedgerAccount ($p_name, $p_type) already exists.")
+	    ;
+	 }
+	 $v_accountID = CloudBankServer::UUID();
+	 CloudBankServer::Singleton()->execQuery(
+	    '
+	       INSERT 
+		  INTO ledger_account(id, name, type) VALUES (:id, :name, :type)
+	    ',
+	    array(':id' => $v_accountID, ':name' => $p_name, ':type' => $p_type)
 	 );
-	 $v_ledgerAccount->id = self::UUID();
-	 $v_ledgerAccount->name = $p_name;
-	 $v_ledgerAccount->type = $p_type;
-
-	 return $v_ledgerAccount;
+	 return $v_accountID;
       }
    }
 ?>
