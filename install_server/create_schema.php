0f9f1c0dda3c79a5407c4aaf1d994fcf79c60c1b
Peter Sági <psagi@freemail.hu>
2010-01-09 17:07:29 +0000

    LedgerAccount -> LedgerAccountService

--- install_server/create_schema.php
+++ install_server/create_schema.php
@@ -2,7 +2,7 @@
 <?php
    require_once(dirname(__FILE__) . '/../server/CloudBankServer.php');
    require_once(dirname(__FILE__) . '/../server/SchemaDef.php');
-   require_once(dirname(__FILE__) . '/../server/LedgerAccount.php');
+   require_once('SCA/SCA.php');
 
    foreach (SchemaDef::CreateSchemaStatements() as $v_sQLStatement) {
 //      try {
@@ -17,5 +17,8 @@
       }
 */
    }
-   LedgerAccount::CreateBeginningAccount();
+   $v_ledgerAccountService = (
+      SCA::getService(dirname(__FILE__) . '/../server/LedgerAccountService.php')
+   );
+   $v_ledgerAccountService->createBeginningAccount();
 ?>
