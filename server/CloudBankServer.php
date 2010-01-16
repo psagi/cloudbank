918bd0de87eb9e36599c2ccff1dd529652f0787f
Peter Sági <psagi@freemail.hu>
2010-01-16 09:29:38 +0000

    Extra "\0" at the end of the UUID fixed, execQuery(): some more debug logging

--- server/CloudBankServer.php
+++ server/CloudBankServer.php
@@ -15,7 +15,7 @@
 	 if (!$v_UUIDGenerator) uuid_create(&$v_UUIDGenerator);
 	 uuid_make($v_UUIDGenerator, UUID_MAKE_V1);
 	 uuid_export($v_UUIDGenerator, UUID_FMT_SIV, &$v_uuid);
-	 return $v_uuid;
+	 return rtrim($v_uuid);	// uuid_export() adds an extra "\0" to the end
       }
 
       private function __construct() {
@@ -38,10 +38,21 @@
 
       public function execQuery($p_sQL, $p_bindArray = NULL) {
 	 $v_statement = $this->r_dBConnection->prepare($p_sQL);
-	 Debug::Singleton()->log(var_export($v_statement, true));
-	 Debug::Singleton()->log(var_export($p_bindArray, true));
+	 Debug::Singleton()->log(
+	    'CloudBankServer::execQuery(): $v_statement = ' .
+	       var_export($v_statement, true)
+	 );
+	 Debug::Singleton()->log(
+	    'CloudBankServer::execQuery(): $p_bindArray = ' .
+	       var_export($p_bindArray, true)
+	 );
 	 $v_statement->execute($p_bindArray);
-	 return $v_statement->fetchAll();
+	 $v_resultSet = $v_statement->fetchAll();
+	 Debug::Singleton()->log(
+	    'CloudBankServer::execQuery(): $v_resultSet = ' .
+	       var_export($v_resultSet, true)
+	 );
+	 return $v_resultSet;
       }
       public function beginTransaction() {
 	 $this->r_dBConnection->beginTransaction();
