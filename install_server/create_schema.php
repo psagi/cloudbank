c517d78b6addb7eef6d1216399dfc2909ce09f07
Peter Sági <psagi@freemail.hu>
2010-01-17 18:44:37 +0000

    if error occurs, print the DB object name along with the error msg

--- install_server/create_schema.php
+++ install_server/create_schema.php
@@ -4,18 +4,18 @@
    require_once(dirname(__FILE__) . '/../server/SchemaDef.php');
    require_once('SCA/SCA.php');
 
-   foreach (SchemaDef::CreateSchemaStatements() as $v_sQLStatement) {
-//      try {
+   foreach (
+      SchemaDef::CreateSchemaStatements() as $v_dBObject => $v_sQLStatement
+   ) {
+      try {
 	 CloudBankServer::Singleton()->execQuery($v_sQLStatement);
-//      }
-/*      
+      }
       catch (PDOException $v_exception) {
 	 exit(
-	    "Execution of SQL statement failed: " . $v_exception->getMessage() .
-	    "\n"
+	    "Execution of SQL statement for creation of '$v_dBObject' " .
+	       "failed: " . $v_exception->getMessage() . "\n"
 	 );
       }
-*/
    }
    $v_ledgerAccountService = (
       SCA::getService(dirname(__FILE__) . '/../server/LedgerAccountService.php')
