49b7dab7ff773c95b57fb203236ca889693cf326
Peter Sági <psagi@freemail.hu>
2010-01-31 10:28:16 +0000

    create*Event() methods simplified to createEvent() using signed amount, modifyEvent(), the logic of swapping the debit and credit accounts has been moved to prepareRelatedAccounts() and is driven by the sign of amount

--- server/EventService.php
+++ server/EventService.php
@@ -22,6 +22,18 @@
 	    )
 	 );
       }
+      private static function IsEqual($p_arr1, $p_arr2, $p_mapping) {
+	 Debug::Singleton()->log(
+	    'EventService::IsEqual(' . var_export($p_arr1, true) . ', ' .
+	       var_export($p_arr2, true) . ', ' . var_export($p_mapping, true) .
+	       ')'
+	 );
+	 foreach ($p_mapping as $v_key1 => $v_key2) {
+	    $v_result = ($p_arr1[$v_key1] == $p_arr2[$v_key2]);
+	    if (!$v_result) break;
+	 }
+	 return $v_result;
+      }
 
       public function __construct() {
 	 $this->r_cloudBankServer = CloudBankServer::Singleton();
@@ -31,60 +43,20 @@
 	 @param string $p_date		The date of the event (YYYY-MM-DD)
 	 @param string $p_description	The description of the event
 	 @param string $p_accountID
-	    The account the amount to be credited/debited from/to
-	 @param string $p_categoryID
-	    The category the amount to be accounted to
-	 @param bool $p_isExpenditure
-	    True if the event is expenditure, false if revenue
-	 @param float $p_amount		The amount
-	 @return bool		Success
-      */
-      public function createExpenditureRevenueEvent(
-	 $p_date, $p_description, $p_accountID, $p_categoryID, $p_isExpenditure,
-	 $p_amount
-      ) {
-	 $this->r_cloudBankServer->beginTransaction();
-	 $this->assertAccountExists($p_accountID);
-	 $this->assertCategoryExists($p_categoryID);
-	 CloudBankServer::SwapIf(
-	    !$p_isExpenditure, $p_categoryID, $p_accountID,
-	    $v_debitLedgerAccountID, $v_creditLedgerAccountID
-	 ); 
-	 $this->createEvent(
-	    $p_date, $p_description, $v_debitLedgerAccountID,
-	    $v_creditLedgerAccountID, $p_amount
-	 );
-	 $this->r_cloudBankServer->commitTransaction();
-	 return true;
-      }
-
-      /**
-	 @param string $p_date		The date of the event (YYYY-MM-DD)
-	 @param string $p_description	The description of the event
-	 @param string $p_startingAccountID
 	    The account the event is started from
 	 @param string $p_otherAccountID
-	    The other account of the transfer
-	 @param bool $p_isWithdrawal
-	    True if the starting account is to be decreased, false if to be
-	    increased
-	 @param float $p_amount		The amount
+	    The other account/category the amount to be debited/credited to/from
+	 @param float $p_amount	
+	    The amount (negative if the starting account's balance is to be
+	    decreased)
 	 @return bool		Success
       */
-      public function createTransferEvent(
-	 $p_date, $p_description, $p_startingAccountID, $p_otherAccountID,
-	 $p_isWithdrawal, $p_amount
+      public function createEvent(
+	 $p_date, $p_description, $p_accountID, $p_otherAccountID, $p_amount
       ) {
 	 $this->r_cloudBankServer->beginTransaction();
-	 $this->assertAccountExists($p_startingAccountID);
-	 $this->assertAccountExists($p_otherAccountID);
-	 CloudBankServer::SwapIf(
-	    $p_isWithdrawal, $p_startingAccountID, $p_otherAccountID,
-	    $v_debitLedgerAccountID, $v_creditLedgerAccountID
-	 ); 
-	 $this->createEvent(
-	    $p_date, $p_description, $v_debitLedgerAccountID,
-	    $v_creditLedgerAccountID, $p_amount
+	 $this->createOrUpdateEvent(
+	    $p_date, $p_description, $p_accountID, $p_otherAccountID, $p_amount
 	 );
 	 $this->r_cloudBankServer->commitTransaction();
 	 return true;
@@ -126,7 +98,31 @@
       }
       
       /**
-	 @param string $p_EventID	The Event to be deleted
+	 @param string $p_accountID	The account the event is started from
+	 @param Event $p_oldEvent http://pety.homelinux.org/CloudBank/EventService
+	 @param Event $p_newEvent http://pety.homelinux.org/CloudBank/EventService
+	 @return bool		Success
+      */
+      public function modifyEvent($p_accountID, $p_oldEvent, $p_newEvent) {
+	 if ($p_newEvent['id'] != $p_oldEvent['id']) {
+	    throw new Exception(
+	       "IDs ({$p_oldEvent['id']}, {$p_newEvent['id']}) must match " .
+		  "when modifying."
+	    );
+	 }
+	 $this->r_cloudBankServer->beginTransaction();
+	 $this->assertSameAsCurrent($p_accountID, $p_oldEvent);
+	 $this->createOrUpdateEvent(
+	    $p_newEvent['date'], $p_newEvent['description'],
+	    $p_accountID, $p_newEvent['other_account_id'],
+	    $p_newEvent['amount'], $p_newEvent['id']
+	 );
+	 $this->r_cloudBankServer->commitTransaction();
+	 return true;
+      }
+
+      /**
+	 @param string $p_eventID	The Event to be deleted
 	 @return bool			Success
       */
       public function deleteEvent($p_eventID) {
@@ -139,9 +135,9 @@
 	 return true;
       }
 
-      public function createEvent(
-	 $p_date, $p_description, $p_debitLedgerAccountID,
-	 $p_creditLedgerAccountID, $p_amount
+      public function createOrUpdateEvent(
+	 $p_date, $p_description, $p_accountID, $p_otherAccountID, $p_amount,
+	 $p_id = NULL
       ) {
 	 if (!SchemaDef::IsValidDate($p_date)) {
 	    throw new Exception("Invalid Event date ($p_date)");
@@ -149,38 +145,52 @@
 	 if (!SchemaDef::IsValidEventDescription($p_description)) {
 	    throw new Exception("Invalid Event description ($p_description)");
 	 }
-	 if (!SchemaDef::IsValidAmount($p_amount)) {
-	    throw new Exception("Invalid amount ($p_amount)");
-	 }
+	 $this->prepareRelatedAccounts(
+	    $p_accountID, $p_otherAccountID, $p_amount, $v_debitLedgerAccountID,
+	    $v_creditLedgerAccountID, $v_amount
+	 );
 	 if (
 	    !(
 	       SchemaDef::IsValidLedgerAccountPair(
-		  $p_debitLedgerAccountID, $p_creditLedgerAccountID
+		  $v_debitLedgerAccountID, $v_creditLedgerAccountID
 	       )
 	    )
 	 ) {
 	    throw new Exception(
-	       "Referenced LedgerAccounts ($p_debitLedgerAccountID) must not " .
+	       "Referenced LedgerAccounts ($v_debitLedgerAccountID) must not " .
 		  "be the same"
 	    );
 	 }
 	 $this->r_cloudBankServer->execQuery(
-	    '
-	       INSERT 
-		  INTO event(
-		     id, date, description, credit_ledger_account_id,
-		     debit_ledger_account_id, amount
-		  ) VALUES (
-		     :id, :date, :description, :credit_ledger_account_id,
-		     :debit_ledger_account_id, :amount
-		  )
-	    ',
+	    (
+	       is_null($p_id) ?
+	       '
+		  INSERT 
+		     INTO event(
+		     	id, date, description, credit_ledger_account_id,
+		       	debit_ledger_account_id, amount
+		     ) VALUES (
+		      	:id, :date, :description, :credit_ledger_account_id,
+			:debit_ledger_account_id, :amount
+		     )
+	       ' :
+	       '
+		  UPDATE event
+		  SET
+		     date = :date, description = :description,
+		     credit_ledger_account_id = :credit_ledger_account_id,
+		     debit_ledger_account_id = :debit_ledger_account_id,
+		     amount = :amount
+		  WHERE id = :id
+	       '
+	    ),
 	    array(
-	       ':id' => CloudBankServer::UUID(), ':date' => $p_date,
+	       ':id' => (is_null($p_id) ? CloudBankServer::UUID() : $p_id),
+	       ':date' => $p_date,
 	       ':description' => $p_description,
-	       ':credit_ledger_account_id' => $p_creditLedgerAccountID,
-	       ':debit_ledger_account_id' => $p_debitLedgerAccountID,
-	       ':amount' => $p_amount
+	       ':credit_ledger_account_id' => $v_creditLedgerAccountID,
+	       ':debit_ledger_account_id' => $v_debitLedgerAccountID,
+	       ':amount' => $v_amount
 	    )
 	 );
       }
@@ -196,16 +206,6 @@
       }
 
       private function __clone() { }
-      private function assertAccountExists($p_accountID) {
-	 $this->assertLedgerAccountExists(
-	    $p_accountID, SchemaDef::LedgerAccountType_Account
-	 );
-      }
-      private function assertCategoryExists($p_categoryID) {
-	 $this->assertLedgerAccountExists(
-	    $p_categoryID, SchemaDef::LedgerAccountType_Category
-	 );
-      }
       private function assertLedgerAccountExists(
 	 $p_ledgerAccountID, $p_type = NULL
       ) {
@@ -253,6 +253,48 @@
 	       "Referenced (non-beginning) Event ($p_eventID) does not exist."
 	    );
 	 }
+      }
+      private function assertSameAsCurrent($p_accountID, $p_oldEvent) {
+	 $v_currentEvent = (
+	    $this->r_cloudBankServer->execQuery(
+	       '
+		  SELECT date, description, other_ledger_account_id, amount
+		  FROM account_events
+		  WHERE ledger_account_id = :account_id AND id = :id
+	       ',
+	       array(':account_id' => $p_accountID, ':id' => $p_oldEvent['id'])
+	    )
+	 );
+	 if (
+	    !self::IsEqual(
+	       $p_oldEvent, $v_currentEvent[0],
+	       array(
+		  'date' => 'date', 'description' => 'description',
+		  'other_account_id' => 'other_ledger_account_id',
+		  'amount' => 'amount'
+	       )
+	    )
+	 ) {
+	    throw new Exception(
+	       "The event to be modified ({$p_oldEvent['id']}) does not " .
+		  "exist or has been modified by another session. Please try " .
+		  "again."
+	    );
+	 }
+      }
+      private function prepareRelatedAccounts(
+	    $p_accountID, $p_otherAccountID, $p_amount_client,
+	    &$p_debitLedgerAccountID, &$p_creditLedgerAccountID, &$p_amount
+	 ) {
+	 $this->assertLedgerAccountExists(
+	    $p_accountID, SchemaDef::LedgerAccountType_Account
+	 );
+	 $this->assertLedgerAccountExists($p_otherAccountID);
+	 CloudBankServer::SwapIf(
+	    $p_amount_client < 0, $p_accountID, $p_otherAccountID,
+	    $p_debitLedgerAccountID, $p_creditLedgerAccountID
+	 ); 
+	 $p_amount = abs($p_amount_client);
       }
 
       private $r_cloudBankServer;
