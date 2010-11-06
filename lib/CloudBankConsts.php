ad0fd27c51333ea2b10613587a2e4bf928f98af1
Peter Sági <psagi@freemail.hu>
2010-11-06 19:58:46 +0000

    Initial revision

---
+++ lib/CloudBankConsts.php
@@ -0,0 +1,9 @@
+<?php
+   class CloudBankConsts {
+      const LedgerAccountType_Account = 'Account';
+      const LedgerAccountType_Category = 'Category';
+      const LedgerAccountType_Beginning = 'Beginning';
+
+      private function __construct() { } // to prevent creating an instance
+   }
+?>
