4ff02519c9b5077e8a9f75617564271a27ac4935
Peter Sági <psagi@freemail.hu>
2010-11-06 21:36:54 +0000

    Initial revision

---
+++ horde_client/cloudbank/accounts.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * $Id: accounts.php,v 1.5 2010/10/24 17:24:02 pety Exp $
+ *
+ * Copyright 2007-2009 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * @author Peter Sagi <psagi@freemail.hu>
+ */
+
+@define('CLOUDBANK_BASE', dirname(__FILE__));
+
+require_once CLOUDBANK_BASE . '/lib/lib/CloudBankConsts.php';
+$g_account_type = CloudBankConsts::LedgerAccountType_Account;
+
+require CLOUDBANK_BASE . '/accounts_or_categories.php';
