148237356813d565a8ae1ba018cba1d695de05f1
Peter Sági <psagi@freemail.hu>
2011-02-03 19:45:20 +0000

    Initial revision

---
+++ util/event_loader.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+
+<?php
+#   require_once(dirname(__FILE__) . '/../server/
+   require_once('SCA/SCA.php');
+
+   $v_eventService = SCA::getService('../server/EventService.php');
+   while (($v_record = fgetcsv(STDIN, 0, '|')) !== FALSE) {
+      $v_eventService->createEvent(
+       	 $v_record[0], $v_record[1], $v_record[2], $v_record[3], $v_record[4]
+      );
+   }
+?>
