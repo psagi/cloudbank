0de9cfe6aaaf72dfb6c73a0d6cc2436071ebf9d6
Peter Sági <psagi@freemail.hu>
2018-10-25 14:12:56 +0200

    Fix: util/event_loader.php adjusted to current Event record structure

--- util/event_loader.php
+++ util/event_loader.php
@@ -7,7 +7,8 @@
    $v_eventService = SCA::getService('../server/EventService.php');
    while (($v_record = fgetcsv(STDIN, 0, '|')) !== FALSE) {
       $v_eventService->createEvent(
-       	 $v_record[0], $v_record[1], $v_record[2], $v_record[3], $v_record[4]
+       	 $v_record[0], $v_record[1], $v_record[2], $v_record[3], $v_record[4],
+	 $v_record[5], $v_record[6], $v_record[7]
       );
    }
 ?>
